e9b2f830197618e0e433bfbfb0279cee15dab1c7
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-09-12 14:50:38 +0200

    Fix converter!!!!!!!!!!!

---
+++ HabitBuilder2/Utilities/HabitBar/Converter/HabitBarWidthConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitBuilder2.Utilities.HabitBar.Converter
+{
+    class HabitBarWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int progress)
+            {
+                
+                return (progress / 21 ) * 20;
+            }
+            if (value is string strValue && int.TryParse(strValue, out int result))
+            {
+               return (result / 21 ) * 20;
+            }
+
+            return 10;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HabitBuilder2/Views/MainPage/Components/MainPageHabitOverviewComponents/HabitBarView.xaml.cs
+++ HabitBuilder2/Views/MainPage/Components/MainPageHabitOverviewComponents/HabitBarView.xaml.cs
@@ -1,13 +1,20 @@
+using Microsoft.Maui.Controls.Shapes;
+
 namespace HabitBuilder2.Views.MainPage.Components.MainPageHabitOverviewComponents;
 
 public partial class HabitBarView : ContentView
 {
     public static readonly BindableProperty HabitTitleProp = BindableProperty.Create(
         nameof(HabitTitle), typeof(string), typeof(HabitBarView), default(string));
+    public static readonly BindableProperty HabitLevelProp = BindableProperty.Create(
+        nameof(HabitLevel), typeof(string), typeof(HabitBarView), default(string));
+    public static readonly BindableProperty HabitStreakProp = BindableProperty.Create(
+        nameof(HabitStreak), typeof(string), typeof(HabitBarView), default(string));
     public HabitBarView()
 	{
     
     InitializeComponent();
+    BindingContext = this;
     }
 
     public string HabitTitle
@@ -15,4 +22,24 @@ public partial class HabitBarView : ContentView
         get => (string)GetValue(HabitTitleProp);
         set => SetValue(HabitTitleProp,value);
     }
+
+    public string HabitLevel
+    {
+        get => (string)GetValue(HabitLevelProp);
+        set => SetValue(HabitLevelProp, value);
+    }
+    public string HabitStreak
+    {
+        get => (string)GetValue(HabitStreakProp);
+        set
+        {
+            SetValue(HabitStreakProp, value);
+            System.Diagnostics.Debug.WriteLine("HabitStreak set: " + value);
+        }
+    }
+    public void AnimateWidth(RoundRectangle loadingBar, double startWidth, double endWidth, uint duration)
+    {
+        loadingBar.Animate("WidthAnimation", d => loadingBar.WidthRequest = d, startWidth, endWidth, length: duration);
+    }
+
 }
