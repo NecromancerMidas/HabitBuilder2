fd3545d26b5f4263e7d4eae28766e0b01dcc9a0e
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-09-21 17:30:33 +0200

    Make viewmodel for Mainpage.

--- HabitBuilder2/MainPage.xaml.cs
+++ HabitBuilder2/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace HabitBuilder2;
+﻿using HabitBuilder2.TempDB;
+using HabitBuilder2.ViewModels.Templates;
+
+namespace HabitBuilder2;
 public partial class MainPage : ContentPage
 {
     public MainPage()
 	{
 		InitializeComponent();
-        
+        var templates = TempDb.Templates.Select(t => new TemplateViewModel(t)).ToList();
     }
 }
 
