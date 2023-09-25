d99c3046dc60d346b6dbf74c5ce9e4aa9df8b0d0
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-09-25 19:36:03 +0200

    Minor Update

--- HabitBuilder2/MainPage.xaml.cs
+++ HabitBuilder2/MainPage.xaml.cs
@@ -1,13 +1,23 @@
-﻿using HabitBuilder2.TempDB;
+﻿using HabitBuilder2.Models;
+using HabitBuilder2.Models.Templates;
+using HabitBuilder2.TempDB;
+using HabitBuilder2.ViewModels;
 using HabitBuilder2.ViewModels.Templates;
 
 namespace HabitBuilder2;
 public partial class MainPage : ContentPage
 {
+    public MainPageViewModel ViewModel;
     public MainPage()
 	{
 		InitializeComponent();
         var templates = TempDb.Templates.Select(t => new TemplateViewModel(t)).ToList();
+        ViewModel = new MainPageViewModel()
+        {
+            
+
+        }
+         
     }
 }
 
