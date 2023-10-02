ed88a2f83bcd58348eaca1da0c4093f3b476339b
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-10-02 14:57:42 +0200

    Added more components, for the second carousel.

---
+++ HabitBuilder2/Views/MainPage/Components/DetailedViewComponents/SpecificHabitOverview.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitBuilder2.Views.MainPage.Components.DetailedViewComponents;
+
+public partial class SpecificHabitOverview : ContentView
+{
+    public SpecificHabitOverview()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HabitBuilder2/Views/MainPage/EditView.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HabitBuilder2.Views.MainPage;
+
+public partial class EditView : ContentView
+{
+    public EditView()
+    {
+        InitializeComponent();
+    }
+}
